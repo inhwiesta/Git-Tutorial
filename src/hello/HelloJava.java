4b1c1004b1504b633866ebcb0df32a20a1195c1b
inhwiesta <inhwiesta@gmail.com>
2020-11-01 00:53:18 +0900

    initial commit

---
+++ src/hello/HelloJava.java
@@ -0,0 +1,17 @@
+/*
+ Data : 2020년 11월 01일
+ Author : 정인휘
+ Description : 첫 번재 자바 프로그램입니다.
+ Version : 1.0
+ */
+
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java"); // 한줄 주석
+
+	}
+
+}
