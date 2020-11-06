95fd7bda30010e293e5bc1e8c905a7c735c8135a
inhwiesta <inhwiesta@gmail.com>
2020-11-06 22:47:49 +0900

    실수형 연습

---
+++ src/chapter2/DoubleEx1.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class DoubleEx1 {
+	public static void main(String[] args) {
+		double dnum = 3.14;
+		float fnum = 3.14F;
+		//F는 식별자이며 float형에 식별자 F는 붙이지 않으면 오류가 발생한다.
+		
+		System.out.println(dnum);
+		System.out.println(fnum);
+		
+	}
+
+}
