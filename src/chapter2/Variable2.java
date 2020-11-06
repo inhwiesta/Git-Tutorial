ef7bbbc6ceeb7b108d1fbb120d00662f91c8dcc2
inhwiesta <inhwiesta@gmail.com>
2020-11-06 21:49:44 +0900

    변수 선언하여 사용하기

--- src/chapter2/Variable2.java
+++ src/chapter2/Variable2.java
@@ -1,11 +1,9 @@
 package chapter2;
 
-public class Variable1 {
+public class Variable2 {
 	public static void main(String[] args) {
-		int level;
-		level = 10;
+		int level = 10;
 		System.out.println(level);
-		
 	}
 	
 	
