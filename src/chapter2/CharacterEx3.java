c5bb7f0d6f3ae4049fa8f8a95aee03c9c75717b4
inhwiesta <inhwiesta@gmail.com>
2020-11-06 22:38:00 +0900

    문자형 연습3

--- src/chapter2/CharacterEx3.java
+++ src/chapter2/CharacterEx3.java
@@ -9,7 +9,7 @@ public class CharacterEx3 {
 		
 		
 		char a2 = 65;
-		//char b2 = -66;
+		//char b2 = -66; 문자형 변수에 음수를 넣으면 오류가 발생한다.
 		
 		System.out.println((char)a);
 		System.out.println((char)b);
